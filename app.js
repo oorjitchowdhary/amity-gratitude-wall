a17a2ef18d6335708adc2e2baad27afd2e625f97
Oorjit Chowdhary <oorjitchowdhary@192.168.1.190>
2022-07-14 17:13:08 +0530

    fetch data from notion + render template

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const express = require('express');
 const path = require('path');
 const { Client } = require('@notionhq/client');
 const notion = new Client({ auth: process.env.NOTION_KEY });
-const needle = require('needle');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
+const databaseId = process.env.NOTION_DATABASE;
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -17,13 +16,76 @@ app.use(express.static(__dirname + '/static'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const database = await notion.databases.query({
+        database_id: databaseId,
+    });
+
+    let wall = [];
+
+    database.results.forEach(async (page) => {
+        let gratitude = [];
+        const pageId = page.id;
+        const pageProperties = page.properties;
+        for (const property in pageProperties) {
+            const propertyValue = await notion.pages.properties.retrieve({ page_id: pageId, property_id: pageProperties[property].id });
+            if (propertyValue.type === 'select') {
+                gratitude.push(propertyValue.select.color, propertyValue.select.name);
+            } else if ('results' in propertyValue) {
+                try {
+                    gratitude.push(propertyValue.results[0].rich_text.plain_text);
+                } catch (e) {
+                    gratitude.push(propertyValue.results[0].title.plain_text);
+                }
+            }
+        }
+
+        console.log(gratitude);
+    });
+
+    console.log(wall);
+
+    res.render('index', { wall: wall });
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('done');
+app.post('/', async (req, res) => {
+    const properties = {
+        email: {
+            title: [{
+                text: { content: req.body.email }
+            }]
+        },
+        name: {
+            rich_text: [{
+                type: "text",
+                text: { content: req.body.name }
+            }]
+        },
+        message: {
+            rich_text: [{
+                type: "text",
+                text: { content: req.body.message }
+            }]
+        },
+        association: {
+            select: {
+                name: req.body.association
+            }
+        }
+    }
+
+    try {
+        await notion.pages.create({
+            parent: {
+                database_id: databaseId
+            },
+            properties: properties
+        });
+
+        return res.redirect('/');
+    } catch (err) {
+        return res.send("failed to add to db");
+    }
 });
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
